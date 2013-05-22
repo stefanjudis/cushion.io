7cbcfcef3007b6ffe7033ebc54b52194c299f164
stefan judis <stefanjudis@gmail.com>
2013-05-22 23:39:38 +0200

    js, html, css for first workin version 8-)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
+;(function(document) {
+  function navigationClickHandler() {
+    var i = 0,
+        id = this.href.split('#')[1],
+        sections = document.querySelectorAll('section'),
+        readme;
 
+
+    if (id) {
+      for(i; i < sections.length; i++){
+        if (sections[i].classList.contains('active')) {
+          sections[i].classList.remove('active');
+        }
+      }
+
+      readme = document.getElementById(id);
+      readme.classList.add('active')
+    }
+  };
+
+  var navigationLinks = document.querySelectorAll('nav a');
+
+  for (var i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener(
+      'click',
+      navigationClickHandler,
+      false
+    );
+  }
+})(document);
